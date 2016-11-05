0c9ca2c7c1d480fe7235645bd3e0f6cdcd276dbd
mattgrosso <mattgrosso@gmail.com>
2016-11-05 15:45:45 -0400

    Created a function for determining next available board.

--- build/js/main.js
+++ build/js/main.js
@@ -34,69 +34,74 @@
         $('.outer.' + outerPosition).children('.' + innerPosition)[0].innerText = myTurn;
         boardWon(boardState[outerPosition], outerPosition);
 
-      }
-
-      if (boardState[outerPosition].boardComplete) {
-        nextBoard = false;
-      } else {
-        nextBoard = innerPosition;
+        whatBoardNext(innerPosition);
       }
 
     });
 
-    //'bo' is a Board Object
-    //'boPosition' is the outer position of bo
-    function boardWon(bo, boPosition) {
-      if ((bo.topLeft) && (bo.topLeft === bo.topCenter) && (bo.topLeft === bo.topRight)) {
-        bo.winner = bo.topLeft;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.middleLeft) && (bo.middleLeft === bo.middleCenter) && (bo.middleLeft === bo.middleRight)) {
-        bo.winner = bo.middleLeft;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.bottomLeft) && (bo.bottomLeft === bo.bottomCenter) && (bo.bottomLeft === bo.bottomRight)) {
-        bo.winner = bo.bottomLeft;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.topLeft) && (bo.topLeft === bo.middleLeft) && (bo.topLeft === bo.bottomLeft)) {
-        bo.winner = bo.topLeft;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.topCenter) && (bo.topCenter === bo.middleCenter) && (bo.topCenter === bo.bottomCenter)) {
-        bo.winner = bo.topCenter;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.topRight) && (bo.topRight === bo.middleRight) && (bo.topRight === bo.bottomRight)) {
-        bo.winner = bo.topRight;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.topLeft) && (bo.topLeft === bo.middleCenter) && (bo.topLeft === bo.bottomRight)) {
-        bo.winner = bo.topLeft;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
-      else if ((bo.topRight) && (bo.topRight === bo.middleCenter) && (bo.topRight === bo.bottomLeft)) {
-        bo.winner = bo.topRight;
-        bo.boardComplete = true;
-        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
-      }
+  //'bo' is a Board Object
+  //'boPosition' is the outer position of bo
+  function boardWon(bo, boPosition) {
+    if ((bo.topLeft) && (bo.topLeft === bo.topCenter) && (bo.topLeft === bo.topRight)) {
+      bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
     }
+    else if ((bo.middleLeft) && (bo.middleLeft === bo.middleCenter) && (bo.middleLeft === bo.middleRight)) {
+      bo.winner = bo.middleLeft;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.bottomLeft) && (bo.bottomLeft === bo.bottomCenter) && (bo.bottomLeft === bo.bottomRight)) {
+      bo.winner = bo.bottomLeft;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.topLeft) && (bo.topLeft === bo.middleLeft) && (bo.topLeft === bo.bottomLeft)) {
+      bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.topCenter) && (bo.topCenter === bo.middleCenter) && (bo.topCenter === bo.bottomCenter)) {
+      bo.winner = bo.topCenter;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.topRight) && (bo.topRight === bo.middleRight) && (bo.topRight === bo.bottomRight)) {
+      bo.winner = bo.topRight;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.topLeft) && (bo.topLeft === bo.middleCenter) && (bo.topLeft === bo.bottomRight)) {
+      bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+    else if ((bo.topRight) && (bo.topRight === bo.middleCenter) && (bo.topRight === bo.bottomLeft)) {
+      bo.winner = bo.topRight;
+      bo.boardComplete = true;
+      $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+    }
+  }
 
-    function whosTurn() {
-      if(currentPlayer === 'X'){
-        currentPlayer = 'O';
-        return 'X';
-      } else {
-        currentPlayer = 'X';
-        return 'O';
-      }
+  function whosTurn() {
+    if(currentPlayer === 'X'){
+      currentPlayer = 'O';
+      return 'X';
+    } else {
+      currentPlayer = 'X';
+      return 'O';
     }
+  }
+
+  // 'innerPosition' is the section of the smaller board that was just clicked on
+  function whatBoardNext(innerPosition) {
+    if (boardState[innerPosition].boardComplete) {
+      nextBoard = false;
+    } else {
+      nextBoard = innerPosition;
+    }
+  }
+
 
 })();
