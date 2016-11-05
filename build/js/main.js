43e4c4ce83664670fcfa3316d8fc1d351d29aed2
mattgrosso <mattgrosso@gmail.com>
2016-11-05 15:04:45 -0400

    Created div for victory on one inner board.

--- build/js/main.js
+++ build/js/main.js
@@ -29,64 +29,56 @@
 
         boardState[outerPosition][innerPosition] = myTurn;
         $('.outer.' + outerPosition).children('.' + innerPosition)[0].innerText = myTurn;
-        boardWon(boardState[outerPosition]);
+        boardWon(boardState[outerPosition], outerPosition);
         nextBoard = innerPosition;
-
-        console.log();
         console.log('next move must be in ', innerPosition);
       }
 
     });
 
     //'bo' is a Board Object
-    function boardWon(bo) {
+    //'boPosition' is the outer position of bo
+    function boardWon(bo, boPosition) {
       if ((bo.topLeft) && (bo.topLeft === bo.topCenter) && (bo.topLeft === bo.topRight)) {
         bo.winner = bo.topLeft;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
+
       }
       else if ((bo.middleLeft) && (bo.middleLeft === bo.middleCenter) && (bo.middleLeft === bo.middleRight)) {
         bo.winner = bo.middleLeft;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.bottomLeft) && (bo.bottomLeft === bo.bottomCenter) && (bo.bottomLeft === bo.bottomRight)) {
         bo.winner = bo.bottomLeft;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.topLeft) && (bo.topLeft === bo.middleLeft) && (bo.topLeft === bo.bottomLeft)) {
         bo.winner = bo.topLeft;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.topCenter) && (bo.topCenter === bo.middleCenter) && (bo.topCenter === bo.bottomCenter)) {
         bo.winner = bo.topCenter;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.topRight) && (bo.topRight === bo.middleRight) && (bo.topRight === bo.bottomRight)) {
         bo.winner = bo.topRight;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.topLeft) && (bo.topLeft === bo.middleCenter) && (bo.topLeft === bo.bottomRight)) {
         bo.winner = bo.topLeft;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
       else if ((bo.topRight) && (bo.topRight === bo.middleCenter) && (bo.topRight === bo.bottomLeft)) {
         bo.winner = bo.topRight;
         bo.boardComplete = true;
-        console.log('We have a winner. It is ', bo.winner);
-        return bo.winner;
+        $('.outer.' + boPosition).addClass(bo.winner + 'Winner');
       }
     }
 
