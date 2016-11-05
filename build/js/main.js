5ca469cc5c06931ab2178adb20f42007e81184bc
mattgrosso <mattgrosso@gmail.com>
2016-11-05 13:01:45 -0400

    Added logic for wins on any given board.

--- build/js/main.js
+++ build/js/main.js
@@ -4,135 +4,75 @@
   var currentPlayer = 'X';
 
   var boardState = {
-    'topLeft': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'topCenter': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'topRight': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'middleLeft': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'middleCenter': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'middleRight': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'bottomLeft': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'bottomCenter': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    },
-    'bottomRight': {
-      'topLeft': null,
-      'topCenter': null,
-      'topRight': null,
-      'middleLeft': null,
-      'middleCenter': null,
-      'middleRight': null,
-      'bottomLeft': null,
-      'bottomCenter': null,
-      'bottomRight': null
-    }
+    'topLeft': {},
+    'topCenter': {},
+    'topRight': {},
+    'middleLeft': {},
+    'middleCenter': {},
+    'middleRight': {},
+    'bottomLeft': {},
+    'bottomCenter': {},
+    'bottomRight': {}
   };
 
   //'bo' is a Board Object
   function boardWon(bo) {
-    if ((bo.topLeft === bo.topCenter) && (bo.topLeft === bo.topRight)) {
+    if ((bo.topLeft) && (bo.topLeft === bo.topCenter) && (bo.topLeft === bo.topRight)) {
       bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
       return bo.winner;
-    } else if (bo.middleLeft && bo.middleCenter && bo.middleRight) {
-      return 'win!';
-    } else if (bo.bottomLeft && bo.bottomCenter && bo.bottomRight) {
-      return 'win!';
-    } else if (bo.topLeft && bo.middleLeft && bo.bottomLeft) {
-      return 'win!';
-    } else if (bo.topCenter && bo.middleCenter && bo.bottomCenter) {
-      return 'win!';
-    } else if (bo.topRight && bo.middleRight && bo.bottomRight) {
-      return 'win!';
-    } else if (bo.topLeft && bo.middleCenter && bo.bottomRight) {
-      return 'win!';
-    } else if (bo.bottomLeft && bo.middleCenter && bo.topRight) {
-      return 'win!';
+    }
+    else if ((bo.middleLeft) && (bo.middleLeft === bo.middleCenter) && (bo.middleLeft === bo.middleRight)) {
+      bo.winner = bo.middleLeft;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.bottomLeft) && (bo.bottomLeft === bo.bottomCenter) && (bo.bottomLeft === bo.bottomRight)) {
+      bo.winner = bo.bottomLeft;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.topLeft) && (bo.topLeft === bo.middleLeft) && (bo.topLeft === bo.bottomLeft)) {
+      bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.topCenter) && (bo.topCenter === bo.middleCenter) && (bo.topCenter === bo.bottomCenter)) {
+      bo.winner = bo.topCenter;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.topRight) && (bo.topRight === bo.middleRight) && (bo.topRight === bo.bottomRight)) {
+      bo.winner = bo.topRight;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.topLeft) && (bo.topLeft === bo.middleCenter) && (bo.topLeft === bo.bottomRight)) {
+      bo.winner = bo.topLeft;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
+    }
+    else if ((bo.topRight) && (bo.topRight === bo.middleCenter) && (bo.topRight === bo.bottomLeft)) {
+      bo.winner = bo.topRight;
+      bo.boardComplete = true;
+      console.log('We have a winner. It is ', bo.winner);
+      return bo.winner;
     }
   }
 
   function whosTurn() {
-    if(currentPlayer === true){
-      currentPlayer = false;
+    if(currentPlayer === 'X'){
+      currentPlayer = 'O';
       return 'X';
     } else {
-      currentPlayer = true;
+      currentPlayer = 'X';
       return 'O';
     }
   }
@@ -141,11 +81,19 @@
     .on('click', function markASquare() {
       var outerPosition = $(this).parent()[0].classList[1];
       var innerPosition = $(this)[0].classList[1];
-      var myTurn = whosTurn();
 
-      boardState[outerPosition][innerPosition] = myTurn;
-      $('.outer.' + outerPosition).children('.' + innerPosition)[0].innerText = myTurn;
+      if (boardState[outerPosition][innerPosition]) {
+        console.log('try again');
+      } else {
+        var myTurn = whosTurn();
 
+        boardState[outerPosition][innerPosition] = myTurn;
+        $('.outer.' + outerPosition).children('.' + innerPosition)[0].innerText = myTurn;
+        boardWon(boardState[outerPosition]);
+
+        console.log();
+        console.log('next move must be in ', innerPosition);
+      }
 
     });
 
