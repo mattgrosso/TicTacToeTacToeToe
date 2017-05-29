eb3000af20061a3667219d05319344ee259d569e
Russell Osborne <russell@burningpony.com>
2017-05-29 13:07:53 -0400

    Add comments on server file.

--- server/app.js
+++ server/app.js
@@ -4,14 +4,20 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 require("./routes/events")(io);
 
+// Log to STDOUT for any request.
 app.use(function(req, res, next) {
   console.log("New request to ", req.path);
+
   next();
 });
 
+// Server static files from the build directory
 app.use(express.static("build"));
+
+// Handle any game route
 app.use("/game", require("./routes/game"));
 
+// Start Server
 const port = process.env.PORT || 3000;
 const binding = "localhost";
 http.listen(port, function() {
